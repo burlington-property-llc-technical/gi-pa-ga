3e32c436f92463035e1df3d243a663c600368b20
Jon McCune <jonmccune@gmail.com>
2025-05-18 21:07:11 -0700

    switch over to a .html file and script its conversion to b.js

--- inject_b.sh
+++ inject_b.sh
@@ -10,10 +10,16 @@ echo "Starting line count of index.html: $(wc -l build_output/index.html)"
 
 # Test if we can modify index.html
 echo "<!-- Jon was here -->" >> build_output/index.html
-sudo cp -i b.js build_output/public/
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
-sed -i -e 's|<footer>|<script src="public/b.js"></script>\n<p>Does text appear?</p>\n      <footer>|' build_output/index.html
+# https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
+echo "document.write('\\" > b.js
+sed 's/\\/\\\\/g;s/^.*$/&\\/g;s/'\''/\\'\''/g' u2_desc.html >> b.js
+echo "');" >> b.js
+sudo cp -i b.js build_output/public/
+
+# The <footer> tag seems a reasonable place in the thumbsup output
+sed -i -e 's|<footer>|<script src="public/b.js"></script>\n      <footer>|' build_output/index.html
 
 echo "Ending line count of index.html: $(wc -l build_output/index.html)"
 
