b2793c8e82ec60486f60c737022827b1305d4633
Jon McCune <jonmccune@gmail.com>
2025-05-18 20:11:30 -0700

    bugfix: add missing closing quote

--- inject_b.sh
+++ inject_b.sh
@@ -7,7 +7,7 @@ ls -la build_output/
 echo "Starting line count of index.html: $(wc -l build_output/index.html)"
 
 # Test if we can modify index.html
-echo "<!-- Jon was here --> >> build_output/index.html
+echo "<!-- Jon was here -->" >> build_output/index.html
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
 sed -i -e 's|<footer>|<script> src="b.js"></script>\n      <footer>|' build_output/index.html
