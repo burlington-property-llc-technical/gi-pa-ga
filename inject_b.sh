7b402875e98187ad4bf6dbd1705dda9780ef9189
Jon McCune <jonmccune@gmail.com>
2025-05-18 20:21:56 -0700

    copy b.js to the output directory, enable 'set -e'

--- inject_b.sh
+++ inject_b.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Exit on first error
+
 # Contents of build_output
 ls -la build_output/
 
@@ -8,6 +10,7 @@ echo "Starting line count of index.html: $(wc -l build_output/index.html)"
 
 # Test if we can modify index.html
 echo "<!-- Jon was here -->" >> build_output/index.html
+sudo cp -i b.js build_output/
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
 sed -i -e 's|<footer>|<script src="b.js"></script>\n      <footer>|' build_output/index.html
