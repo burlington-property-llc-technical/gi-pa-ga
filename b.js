5cdddfa66c948422ab3e64fc881091a4b17e0e14
Jon McCune <jonmccune@gmail.com>
2025-05-18 20:32:35 -0700

    bugfix: balance single quotes

--- b.js
+++ b.js
@@ -3,7 +3,7 @@ document.write('\
 \
     <h1>Add your HTML code here</h1>\
 \
-     <p>Notice however, that you have to escape LF's with a '\', just like\
+     <p>Notice however, that you have to escape LFs with a "\", just like\
         demonstrated in this code listing.\
     </p>\
 \
