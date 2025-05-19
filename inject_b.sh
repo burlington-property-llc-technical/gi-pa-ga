b35279d5e85a87b0b00c3c1d5d05cc6fb1c8108a
Jon McCune <jonmccune@gmail.com>
2025-05-18 20:29:53 -0700

    put b.js in the public/ directory with other javascript

--- inject_b.sh
+++ inject_b.sh
@@ -10,10 +10,10 @@ echo "Starting line count of index.html: $(wc -l build_output/index.html)"
 
 # Test if we can modify index.html
 echo "<!-- Jon was here -->" >> build_output/index.html
-sudo cp -i b.js build_output/
+sudo cp -i b.js build_output/public/
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
-sed -i -e 's|<footer>|<script src="b.js"></script>\n      <footer>|' build_output/index.html
+sed -i -e 's|<footer>|<script src="public/b.js"></script>\n      <footer>|' build_output/index.html
 
 echo "Ending line count of index.html: $(wc -l build_output/index.html)"
 
