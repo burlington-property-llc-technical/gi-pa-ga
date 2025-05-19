97d6f22d01438381fb9487348cd0b1b3b08e8d9f
Jon McCune <jonmccune@gmail.com>
2025-05-18 20:38:46 -0700

    add a <p> sentence

--- inject_b.sh
+++ inject_b.sh
@@ -13,7 +13,7 @@ echo "<!-- Jon was here -->" >> build_output/index.html
 sudo cp -i b.js build_output/public/
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
-sed -i -e 's|<footer>|<script src="public/b.js"></script>\n      <footer>|' build_output/index.html
+sed -i -e 's|<footer>|<script src="public/b.js"></script>\n<p>Does text appear?</p>\n      <footer>|' build_output/index.html
 
 echo "Ending line count of index.html: $(wc -l build_output/index.html)"
 
