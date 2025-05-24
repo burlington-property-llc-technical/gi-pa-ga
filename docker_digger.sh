33f20daa0331a51b505b9b9a1f47ea1d0373736a
Jonathan McCune <jonmccune@gmail.com>
2025-05-24 15:39:53 -0700

    still hunting for thumbsup binary

--- docker_digger.sh
+++ docker_digger.sh
@@ -6,13 +6,13 @@ echo "=== My environment ==="
 printenv
 
 echo "=== PWD: $PWD ==="
-
+ls -la
+ls -la /
+which thumbsup
 OLDIFS=$IFS
 IFS=':'
 for dir in $PATH; do
     ls -la *thumb* || true
 done
 IFS=$OLDIFS
-ls -la
-ls -la /
 
