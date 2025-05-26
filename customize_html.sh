0108c8ca698ad788ecdefc5f11b8c4276992e0f9
Jon McCune <jonmccune@gmail.com>
2025-05-25 19:19:10 -0700

    restructure content injection towards reusability

--- customize_html.sh
+++ customize_html.sh
@@ -13,13 +13,14 @@ echo "<!-- Jon was here -->" >> build_output/index.html
 
 # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
 # https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
-echo "document.write('\\" > b.js
-sed 's/\\/\\\\/g;s/^.*$/&\\/g;s/'\''/\\'\''/g' u2_desc.html >> b.js
-echo "');" >> b.js
-sudo cp -i b.js build_output/public/
+JSFILE=index_customizer.js
+echo "document.write('\\" > ${JSFILE}
+sed 's/\\/\\\\/g;s/^.*$/&\\/g;s/'\''/\\'\''/g' index_desc.html >> ${JSFILE}
+echo "');" >> ${JSFILE}
+sudo cp -i ${JSFILE} build_output/public/
 
 # The <footer> tag seems a reasonable place in the thumbsup output
-sed -i -e 's|<footer>|<script src="public/b.js"></script>\n      <footer>|' build_output/index.html
+sed -i -e 's|<footer>|<script src="public/'"${JSFILE}"'"></script>\n      <footer>|' build_output/index.html
 
 echo "Ending line count of index.html: $(wc -l build_output/index.html)"
 
