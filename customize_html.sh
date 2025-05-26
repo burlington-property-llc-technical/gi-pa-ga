3f004b507603203a1af29ff45a24f687a0172017
Jon McCune <jonmccune@gmail.com>
2025-05-25 19:47:27 -0700

    bash function arguments are 1-indexed

--- customize_html.sh
+++ customize_html.sh
@@ -5,14 +5,12 @@ set -x  # Echo invoked commands
 
 # inject_html target_file contents_file
 inject_html() {
-    TARGET=$0
-    CONTENTS=$1
+    TARGET=$1
+    CONTENTS=$2
     echo "inject_html invoked: ${TARGET} ${CONTENTS}"
 
     JSFILE=$(echo "$CONTENTS" | sed -e 's/[.]html$/.js/')
     echo "Creating ${JSFILE}"
-
-
 
     # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
     # https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
@@ -25,26 +23,12 @@ inject_html() {
     sed -i -e 's|<footer>|<script src="public/'"${JSFILE}"'"></script>\n      <footer>|' ${TARGET}
 }
 
-# Contents of build_output
-ls -la build_output/
-
 # Prove we can access build_output/index.html
 echo "Starting line count of index.html: $(wc -l build_output/index.html)"
 
-# Test if we can modify index.html
-echo "<!-- Jon was here -->" >> build_output/index.html
-
-# # https://stackoverflow.com/questions/7189604/replacing-html-tag-content-using-sed
-# # https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
-# JSFILE=index_customizer.js
-# echo "document.write('\\" > ${JSFILE}
-# sed 's/\\/\\\\/g;s/^.*$/&\\/g;s/'\''/\\'\''/g' index_desc.html >> ${JSFILE}
-# echo "');" >> ${JSFILE}
-# sudo cp -i ${JSFILE} build_output/public/
-
-# # The <footer> tag seems a reasonable place in the thumbsup output
-# sed -i -e 's|<footer>|<script src="public/'"${JSFILE}"'"></script>\n      <footer>|' build_output/index.html
-
 inject_html "build_output/index.html" "index_desc.html"
+
+# Contents of build_output
+ls -la build_output/
 
 echo "Ending line count of index.html: $(wc -l build_output/index.html)"
