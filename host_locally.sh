05f944a464efb58734c18ce611b81c4a604dfcb0
Jon McCune <jonmccune@gmail.com>
2025-06-01 17:56:27 -0700

    sort of works

--- host_locally.sh
+++ host_locally.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "!!! UNTESTED !!! UNTESTED !!! UNTESTED !!!"
+set -e
+
+# Don't delete - use wget -N instead
+# https://stackoverflow.com/questions/77050692/how-to-redownload-file-with-wget-only-when-the-file-is-newer-or-the-size-chang
+# rm -rf tmp/burlingtonpropertyllc.com
 
 # Archive a copy of the live site
 # https://stackoverflow.com/questions/12934528/recursive-wget-with-hotlinked-requisites
 # TODO(jonmccune): figure out partial update, no reason to actually transfer media files
 # we already have locally.
-wget -e robots=off -r -l inf -pk -H https://burlingtonpropertyllc.com
+wget -N -P tmp -e robots=off -r -l inf -pk -H https://burlingtonpropertyllc.com
 
 # https://browsersync.io/#install
-npx browser-sync start --server --files burlingtonpropertyllc.com/public/theme.css
+(cd tmp/burlingtonpropertyllc.com && npx browser-sync start --server --files tmp/burlingtonpropertyllc.com/public/theme.css)
+
